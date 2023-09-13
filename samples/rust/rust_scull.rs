640fea7b09baad8e2940a822689fb2df2c4a122e
Gwan-gyeong Mun <gwan-gyeong.mun@intel.com>
2023-09-14 00:01:32 +0300

    Step 9: save data written / return saved data
    data access is protected by Mutex
    
    [Test commands]
    
    1. module load
       insmod /lib/modules/`uname -r`/kernel/samples/rust/rust_scull.ko
    
    2. write test
       cat > /dev/scull
    3. read test
    
       cat /dev/scull
    
    Signed-off-by: Gwan-gyeong Mun <gwan-gyeong.mun@intel.com>

--- samples/rust/rust_scull.rs
+++ samples/rust/rust_scull.rs
@@ -8,7 +8,9 @@ use kernel::io_buffer::{IoBufferWriter,
                         IoBufferReader};
 //use kernel::sync::Ref;
 use kernel::sync::{Arc,
-                   ArcBorrow};
+                   ArcBorrow,
+                   UniqueArc,
+                   Mutex};
 
 module! {
     type: Scull,
@@ -18,9 +20,43 @@ module! {
     license: "GPL",
 }
 
-
+// latest version will have new pin macros
+//#[pin_data]
 struct Device {
     number: usize,
+    // latest version will have new pin macros
+    //#[pin]
+    contents: Mutex<Vec<u8>>,
+}
+
+// latest version will have new pin macros style for initialization
+impl Device {
+    /*
+       // Allocate a boxed `Device`.
+	   // let d = Box::pin_init(Device::try_new())?;
+
+       // will Arc have Arc::pin_init? => needs to check below
+       // let d = Arc::pin_init(Device::try_new())?;
+
+    fn try_new() -> impl PinInit<Self> {
+        pin_init!(Self {
+            number: 10,
+            contents <- new_mutex!(Vec::new()),
+        })
+    */
+    fn try_new() -> Result<Arc<Self>> {
+        let mut dev = Pin::from(UniqueArc::try_new(Self {
+            number: 0,
+            // SAFETY: `mutex_init!` is called below.
+            contents: unsafe {Mutex::new(Vec::new())},
+        })?);
+
+        // SAFETY: `connents' is pinned when `dev` is.
+        let pinned = unsafe {dev.as_mut().map_unchecked_mut(|s| &mut s.contents) };
+        kernel::mutex_init!(pinned, "Device::contents");
+
+        Ok(dev.into())
+    }
 }
 
 // latest version will have new pin macros
@@ -40,6 +76,7 @@ impl file::Operations for Scull {
     fn open(context:&Arc<Device>, _file: &file::File) -> Result<Arc<Device>> {
         // context.number deref coercion does not work on rust-analyzer
         pr_info!("File for device {} was opened\n", context.number);
+
         Ok(context.clone())
     }
 
@@ -59,12 +96,20 @@ impl file::Operations for Scull {
     */
     fn read(data: ArcBorrow<'_, Device>,
             _file: &file::File,
-            _writer: &mut impl IoBufferWriter,
-            _offset: u64,
+            writer: &mut impl IoBufferWriter,
+            offset: u64,
     ) -> Result<usize> {
         // data.number deref coercion does not work on rust-analyzer
-        pr_info!("File for device {} was read\n", data.number);
-        Ok(0)
+        //pr_info!("File for device {} was read\n", data.number);
+
+        let offset = offset.try_into()?;
+        let vec = data.contents.lock();
+        let len = core::cmp::min(writer.len(), vec.len().saturating_sub(offset));
+        writer.write_slice(&vec[offset..][..len])?;  // => is it the combination of &vec[offset..] and &vec[..len] ?
+        //writer.write_slice(&vec[offset..len])?;  // => what is the difference between &vec[offset..][..len] and &vec[offset..len] ?
+        pr_info!("File for device {} was read size: {}\n", data.number, len);
+
+        Ok(len)
     }
 
     /*
@@ -82,8 +127,15 @@ impl file::Operations for Scull {
              reader: &mut impl IoBufferReader,
              _offset: u64
     ) -> Result<usize> {
-        pr_info!("File for device {} was written\n", data.number);
-        Ok(reader.len())
+        //pr_info!("File for device {} was written\n", data.number);
+        let copy = reader.read_all()?;
+
+        let mut contents = data.contents.lock();
+        *contents = copy;
+
+        pr_info!("File for device {} was written size: {}\n", data.number, contents.len());
+
+        Ok(contents.len())
     }
 
 }
@@ -94,7 +146,8 @@ impl kernel::Module for Scull {
         // latest version will have new pin macros
         // such as ... Box::pin_init(miscdev::Registration::new(fmt!("scull"), ()))?;
         // new_pinned(name: fmt::Arguments<'_>, open_data: T::OpenData) -> Result<Pin<Box<Self>>>
-        let dev = Arc::try_new(Device {number: 0})?;
+
+        let dev = Device::try_new()?;
         let reg = miscdev::Registration::new_pinned(fmt!("scull"), dev)?;
 
         Ok(Self{_dev: reg})
