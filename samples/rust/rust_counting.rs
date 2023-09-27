170e553e5d45ad0f3cd079a027f4075cd5360983
Gwan-gyeong Mun <gwan-gyeong.mun@intel.com>
2023-09-27 12:29:41 +0300

    Training: Pin-init
    [slide deck]
      https://kangrejos.com/2023/The%20%60pin-init%60%20API%20Training.pdf
    
    [reference]
      https://github.com/Rust-for-Linux/linux/compare/rust-next...y86-dev:linux:kangrejos-2023-training-solution
    
    // #[pin] macro makes the its below fields wraps pinned_data, and implements pin impl for the variable.
    // The variable on the line after the #[pin] macro can be initialized by pin_init!() macro,
       which returns PinInit<T>. And this return object is used as an argument to Box::pin_init().
    
    Open: why does it need to add allocator_api to rust_allowed_features?
    Answer: allocator_api has nothing to do with global allocators.
    	There are quite a few APIs in Box that requires allocator_api:
    	https://doc.rust-lang.org/stable/std/boxed/struct.Box.html
    
    Signed-off-by: Gwan-gyeong Mun <gwan-gyeong.mun@intel.com>

--- samples/rust/rust_counting.rs
+++ samples/rust/rust_counting.rs
@@ -1,10 +1,15 @@
 // SPDX-License-Identifier: GPL-2.0
 
 #![allow(dead_code)]
-//#![allow(unused_imports)]
+#![allow(unused_imports)]
 //! Rust counting example for Kangrejos
 
-use kernel::prelude::*;
+use kernel::{
+    init::{self, PinnedDrop},
+    new_mutex,
+    prelude::*,
+    sync::Mutex,
+};
 
 module! {
     type: RustCounting,
@@ -16,8 +21,96 @@ module! {
 
 struct RustCounting;
 
+#[pin_data]
+struct NamedCounter {
+    name: &'static str,
+    #[pin]
+    value: Mutex<(i32, i32)>,
+}
+
+impl NamedCounter {
+    fn new(name: &'static str, cur_value: i32, max_value: i32) -> impl PinInit<Self> {
+        pin_init!(Self {
+            name,
+            value <- new_mutex!((cur_value, max_value)),
+        })
+    }
+
+    fn increment(&self) -> bool {
+        let mut val = self.value.lock();
+        //let mut val: (i32, i32) = self.value.lock();
+
+        val.0 += 1;
+        val.0 >= val.1
+    }
+
+    fn value(&self) -> i32 {
+        self.value.lock().0
+    }
+
+    fn set_max(&self, max_value: i32) {
+        self.value.lock().1 = max_value;
+    }
+}
+
+#[pin_data(PinnedDrop)]
+struct MonitorBuffer {
+    #[pin]
+    read: NamedCounter,
+    #[pin]
+    write: NamedCounter,
+    buf: Box<[u8;100_000]>,
+}
+
+impl MonitorBuffer {
+    fn new() -> impl PinInit<Self, Error> {
+        try_pin_init!(Self {
+            read <- NamedCounter::new("Read Counter", 0, i32::MAX),
+            write <- NamedCounter::new("Write Counter", 0, i32::MAX),
+            buf: Box::init(init::zeroed())?,
+        })
+    }
+
+    fn set(&mut self, idx: usize, val: u8) {
+        self.write.increment();
+        self.buf[idx] = val;
+    }
+
+    fn get(&self, idx: usize) -> u8 {
+        self.read.increment();
+        self.buf[idx]
+    }
+}
+
+#[pinned_drop]
+impl PinnedDrop for MonitorBuffer {
+    fn drop(self: Pin<&mut Self>) {
+        pr_info!(
+            "Monitored Buffer was read {} times and written to {} times.",
+            self.read.value(),
+            self.write.value()
+        )
+    }
+}
+
+#[derive(Zeroable,Debug)]
+struct LotsOfData {
+    a_buf: [u8;128],
+    b_buf: [u8;256],
+    mode: i32,
+    count: usize,
+    data: *mut u8,
+    len: usize,
+}
+
 impl kernel::Module for RustCounting {
     fn init(_module: &'static ThisModule) -> Result<Self> {
+        let data = Box::init(init!(LotsOfData {
+            mode: 8,
+            ..Zeroable::zeroed()
+        }));
+        pr_info!("{data:?}");
+
         Ok(Self)
     }
 }
